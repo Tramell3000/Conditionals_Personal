2a3aa43e62434d66af21319bb54a73d531f49120
TramellCutler <TramellCutler@fullsail.edu>
2014-04-17 16:38:36 -0400

    commit
    change in commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-var age = 30;
+var minAge = 30;
+var minAgewescort = 29;
 
-if(age < 30){
-	console.log("Will not be able to enter!");
+
+if(minAge < 30){
+	console.log("Will be able to enter!");
+
+}else if(minAge > minAgewescort){
+	console.log("You can enter but, with escort only!");
 
 }else{
-	console.log("Will be able to enter!");
+	console.log("Will not be able to enter!");
 }
 
 
